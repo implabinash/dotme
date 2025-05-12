290fb8120b7541b10b836720af35a9b14d5c78c0
Abinash Sahoo <implabinash@gmail.com>
2025-05-12 17:37:29 +0530

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<section class="mx-auto h-full p-10 md:w-[70%] xl:w-[40%]">
+	<img src="/images/dog-park.png" alt="404" class="" />
+	<h1 class="text-center text-9xl font-bold">404</h1>
+	<h1 class="text-center text-2xl font-bold">Page not found.</h1>
+</section>
